7f4b7050fd46706aedb3f61171ad9ec95102662f
AliIMRAN017 <meali0959@gmail.com>
2026-06-25 17:58:34 +0500

    Create Student.h

---
+++ Student.h
@@ -0,0 +1,24 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+
+#include <string>
+using namespace std;
+
+struct Student {
+    string roll;
+    string name;
+    string dept;
+    string semester;
+    double cgpa;
+    string status; 
+};
+
+void addStudent();
+bool searchByRoll(const string& rollNo, Student& student);
+void searchByName(const string& subStr);
+void updateStudent(const string& rollNo, int fieldOption, const string& newValue);
+void softDelete(const string& rollNo);
+void listActiveStudents();
+bool validateRollNumber(const string& rollNo);
+
+#endif
